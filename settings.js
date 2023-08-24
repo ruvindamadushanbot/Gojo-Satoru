05ed3a6ca90b5add74e210a6b2c65ccfbcaee8d4
ruvindamadushanbot <118910419+ruvindamadushanbot@users.noreply.github.com>
2023-08-24 18:24:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94702650071']
+global.premium = ['94702650071']
+global.ownernomer = '94702650071'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
